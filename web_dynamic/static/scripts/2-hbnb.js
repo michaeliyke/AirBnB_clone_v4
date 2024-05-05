93afba726bbb571a49670dc24ebbfbf63c9bf1db
Michael Iyke <ymichaelc@gmail.com>
2024-05-05 21:48:55 +0100

    Task3: js part

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -4,6 +4,7 @@ const jQuery = $;
 jQuery(function ($) {
   const amenities = {};
 
+  // 0: Deals with the Amenities dropdown checkboxes section
   $('input[type="checkbox"]').change(function () {
     const id = $(this).data('id');
     const name = $(this).data('name');
@@ -19,5 +20,18 @@ jQuery(function ($) {
     } else {
       $('.amenities h4').html('&nbsp;');
     }
-  });
+  }); /* end 0: */
+
+  // 1: Deals with the API status indicator
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (status === 'success') {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+        return;
+      }
+    }
+    $('#api_status').removeClass('available');
+  }); /* end 1: */
+
+
 });
